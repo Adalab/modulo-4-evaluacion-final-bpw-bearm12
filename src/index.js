0df931f37f0c09ed6ab76cc6558658d747be884a
Bea Ruf <bearm12493@gmail.com>
2024-04-19 09:17:48 +0100

    endpoint get

--- src/index.js
+++ src/index.js
@@ -23,3 +23,41 @@ const port = process.env.PORT || 4001;
 app.listen(port, () => {
   console.log("Server is listening in http://localhost:" + port);
 });
+
+//ENDPOINTS
+
+app.get("/games", async (req, res) => {
+  const connection = await getDBConnection();
+
+  const querySQL =
+    "SELECT id, title, cover, year, dev, genre FROM games, genre WHERE idGenre = fk_genre";
+  const [result] = await connection.query(querySQL);
+  connection.end();
+
+  res.status(200).json({
+    info: { success: true, count: result.length },
+    results: result,
+  });
+});
+
+app.get("/games/:id", async (req, res) => {
+  const idGame = req.params.id;
+  const connection = await getDBConnection();
+
+  const querySQL =
+    "SELECT id, title, cover, year, dev, genre FROM games, genre WHERE idGenre = fk_genre AND id = ?";
+  const [result] = await connection.query(querySQL, [idGame]);
+  connection.end();
+
+  if (result.length === 0) {
+    res.status(404).json({
+      success: false,
+      error: "No hay ningún juego con ese id",
+    });
+  } else {
+    res.status(200).json({
+      success: true,
+      result: result,
+    });
+  }
+});
